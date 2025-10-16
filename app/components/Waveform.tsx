dbffee7d10b683acb4ac64a3ddf3e292f1b30653
Kyle Karpack <karpack@adobe.com>
2025-10-16 11:23:01 -0700

    Improve typing and start adding editing

--- app/components/Waveform.tsx
+++ app/components/Waveform.tsx
@@ -1,13 +1,32 @@
 import WavesurferPlayer from "@wavesurfer/react";
 import { useState } from "react";
+import WaveSurfer from "wavesurfer.js";
 
 export default function Waveform({ audioUrl }: { audioUrl: string }) {
-  const [wavesurfer, setWavesurfer] = useState<any>(null);
+  const [wavesurfer, setWavesurfer] = useState<WaveSurfer | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const onReady = (ws: any) => {
+  const onPlay = () => {
+    const context = new AudioContext();
+    const compressor =context.createDynamicsCompressor();
+    compressor.threshold.value = -10;
+    compressor.knee.value = 40;
+    compressor.ratio.value = 12;
+    compressor.attack.value = 0;
+    compressor.release.value = 0.25;
+    
+    const audio = wavesurfer?.getMediaElement();
+    const mediaNode = context.createMediaElementSource(audio!);
+    mediaNode.connect(compressor);
+    compressor.connect(context.destination);
+  }
+
+  const onReady = (ws: WaveSurfer) => {
     setWavesurfer(ws);
     setIsPlaying(false);
+
+    // TODO: Effects
+    // ws.once("play", onPlay);
   };
 
   const onPlayPause = () => {
@@ -25,6 +44,7 @@ export default function Waveform({ audioUrl }: { audioUrl: string }) {
         onPlay={() => setIsPlaying(true)}
         onPause={() => setIsPlaying(false)}
         mediaControls={true}
+        normalize={true}
       />
     </>
   );
